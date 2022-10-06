d61e87c8b6c3154dbc9a53ec1b451f5acf8b9df1
RusElfimov <ruselfimovbox@outlook.com>
2022-10-06 23:08:49 +0300

    work2

---
+++ work2/Program.cs
@@ -0,0 +1,14 @@
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+Console.WriteLine("Введите первое число");
+int n1 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int n2 = int.Parse(Console.ReadLine());
+
+if (n1==n2*n2)
+{
+    Console.Write("Первое число - квадрат второго");
+}
+else 
+{
+    Console.Write("Первое число не является квадратом второго");
+} 
