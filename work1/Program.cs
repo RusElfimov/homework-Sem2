0b5a4460704a407ab15c8884619f1a96e9823a9e
RusElfimov <ruselfimovbox@outlook.com>
2022-10-06 23:01:53 +0300

    work1

---
+++ work1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным.
+Console.WriteLine("Введите число");
+int n = int.Parse(Console.ReadLine());
+if (n>0)
+    if (n<=5)
+    {
+    Console.Write("Рабочий день");
+    }
+else if (n==6)
+
+{
+   Console.Write("Выходной"); 
+}
+
+else if (n==7)
+
+{
+   Console.Write("Выходной день"); 
+}
+
+else Console.Write("Это не день недели");
+
