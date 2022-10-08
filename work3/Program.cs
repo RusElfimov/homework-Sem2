27e35825a7515a58748b0bdfaab8184247c6f12c
RusElfimov <ruselfimovbox@outlook.com>
2022-10-08 11:27:32 +0300

    work3

---
+++ work3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Задать номер четверти, показать диапазоны для возможных координат.
+Console.WriteLine("Ввести номер четверти в плоскости");
+int N = int.Parse(Console.ReadLine());
+
+if (N==1)
+{
+    Console.WriteLine("x>0, y>0");
+} 
+
+else if (N==2)
+{
+    Console.WriteLine("x<0, y>0");
+} 
+
+else if (N==3)
+{
+    Console.WriteLine("x<0, y<0");
+} 
+
+else if (N==4)
+{
+    Console.WriteLine("x>0, y<0");
+} 
+
+ 
+else 
+{
+    Console.WriteLine("Это не четверть");
+}
